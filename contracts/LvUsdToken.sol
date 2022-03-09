3af37bd97407cfbb741de52b62eea0643b6c0368
tomerXarchimedes <tomer@archimedesfi.com>
2022-03-09 15:43:16 -0800

    Updating LVUSD token contract
    including test and strawman

--- contracts/LvUsdToken.sol
+++ contracts/LvUsdToken.sol
@@ -1,11 +1,8 @@
 pragma solidity 0.8.4;
 
-
-//import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
-import {ERC20} from "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/access/AccessControl.sol";
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import {SafeERC20} from "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
-import "@openzeppelin/contracts/access/Ownable.sol";
-
 
 /// @title lvUSD token
 ///
@@ -13,29 +10,55 @@ import "@openzeppelin/contracts/access/Ownable.sol";
 ///
 /// Not implementation just external facing interfaces
 ///
-contract LvUsdToken is ERC20("Archimedes lvUSD", "lvUSD"), Ownable {
+contract LvUsdToken is ERC20, AccessControl {
   using SafeERC20 for ERC20;
 
+  /* Access Control */
+  bytes32 public constant MINTER_ROLE = keccak256("MINTER_ROLE");
 
+  constructor() ERC20("Archimedes lvUSD", "lvUSD") {
 
+      // Grant the contract deployer the default admin role and minter role
+      _grantRole(DEFAULT_ADMIN_ROLE, msg.sender);
+      _grantRole(MINTER_ROLE, msg.sender);
+  }
 
+  /// @dev Set minter
   ///
-  /// This function reverts if the caller does not have the minter role.
+  /// Set minter
+  ///
+  /// @param _minter new minter address
+ function setMinter(address _minter) external {
+   require(hasRole(DEFAULT_ADMIN_ROLE, msg.sender), "Caller is not an admin");
+   grantRole(MINTER_ROLE, _minter);
+ }
+
+ /// @dev Revoke minter
+ ///
+ /// Set minter
+ ///
+ /// @param _minter to revoke minter address
+ function revokeMinter(address _minter) external {
+   require(hasRole(DEFAULT_ADMIN_ROLE, msg.sender), "Caller is not an admin");
+   revokeRole(MINTER_ROLE, _minter);
+ }
+
+  /// @dev mint new lvUSD tokens
+  ///
+  /// This function reverts if the caller does not have the Owner role.
   ///
   /// @param _recipient the account to mint tokens to.
   /// @param _amount    the amount of tokens to mint.
-  function mint(address _recipient, uint256 _amount) onlyOwner external  {
+  function mint(address _recipient, uint256 _amount) external  {
 
-    // extra check - just owner can mint and only to itself
-    require(_recipient == owner());
-
+    // Access Control
+    require(hasRole(MINTER_ROLE, msg.sender), "Caller is not a minter");
 
     _mint(_recipient, _amount);
 
-
   }
 
-
-
-
 }
+
+
+/* TODO: Add timelock on admin actions */
--- test/LvUsdToken.js
+++ test/LvUsdToken.js
@@ -21,6 +21,7 @@ describe("LVUSD Token contract", function () {
      // for it to be deployed(), which happens once its transaction has been
      // mined.
      lvToken = await lvTokenContract.deploy();
+
    });
 
 
@@ -30,7 +31,7 @@ describe("LVUSD Token contract", function () {
   });
 
 
-  it("Owner can mint", async function () {
+  it("Default Minter can mint", async function () {
 
     let mint_amount = 100;
 
@@ -38,16 +39,49 @@ describe("LVUSD Token contract", function () {
 
     expect(await lvToken.totalSupply()).to.equal(mint_amount);
     expect(await lvToken.balanceOf(owner.address)).to.equal(mint_amount);
-
   });
+
 
   it("Non owner cannot mint", async function () {
 
     let mint_amount = 200;
-    
-    await expect(lvToken.connect(addr1).mint(owner.address, mint_amount)).to.be.revertedWith('Ownable: caller is not the owner');
+
+    await expect(lvToken.connect(addr1).mint(owner.address, mint_amount)).to.be.revertedWith('Caller is not a minter');
 
   });
+
+  it("Grant minter role and mint", async function () {
+
+    let mint_amount = 200;
+
+    await expect(lvToken.connect(addr1).mint(owner.address, mint_amount)).to.be.revertedWith('Caller is not a minter');
+
+    // set address 1 as minter
+    lvToken.setMinter(addr1.address)
+
+    // revoking previous minter
+    lvToken.revokeMinter(owner.address)
+
+    // addr1 should be able to mint
+    lvToken.connect(addr1).mint(addr1.address, mint_amount)
+    expect(await lvToken.connect(addr1).balanceOf(addr1.address)).to.equal(mint_amount);
+
+    // previous minter cannot mint
+    await expect(lvToken.mint(owner.address, mint_amount)).to.be.revertedWith('Caller is not a minter');
+
+  });
+
+  it("Decimal must be 18", async function () {
+
+    expect(await lvToken.decimals()).to.equal(18);
+
+
+  });
+
+  /*
+  1. No one can burn
+  2. Approve + transfer tests
+  */
 
 
 });
