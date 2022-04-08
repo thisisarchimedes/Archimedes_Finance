492ad8d66c12079afc650bc3e375f71c4c46482c
tomerXarchimedes <98904111+tomerXarchimedes@users.noreply.github.com>
2022-04-08 10:17:45 -0700

    Dev ops (#4)
    * Local network is now a mainnet fork by default
    
    locked block number to improve performance (we'll update it every now or than)
    Otherwise, we now run all tests on a mainnet fork
    
    Live reviewed by: @yotamdaniel
    
    * Added basic pre-filght tests
    
    Reviewed by: @yotamdaniel  and @jasonstockman
    
    * Add a skeleton integration tests
    
    Should run last (therefore the "z" at the begining)
    added an helper file with all the on-chain manipulations (only ETH->OUSD for now)
    
    reviewed by @jasonstockman & @yotamdaniel
    
    * Changing the name of the GitHub Action file
    
    Because we are going to add more - yey!
    
    * Fixing private api key exposure
    
    Add a submodule (a ref to a private github)
    
    * Remove un-needed "solidity coverage"
    
    * Moved all consts to a single file
    
    * Exported consts (ABIs and contract addresses) from MainnetHelper
    * Also fixed a filename typo (MainnerHelper->MainnetHelper)
    
    * adding "dotenv" npm
    
    adding "dotenv" npm
    
    * Update BuildAndTest.yml
    
    * Update BuildAndTest.yml
    
    * Update BuildAndTest.yml
    
    * Update BuildAndTest.yml
    
    * Update BuildAndTest.yml
    
    * Update BuildAndTest.yml
    
    * Update BuildAndTest.yml
    
    * Update BuildAndTest.yml
    
    * Update BuildAndTest.yml
    
    * Update BuildAndTest.yml
    
    * Update BuildAndTest.yml
    
    * Update BuildAndTest.yml
    
    * Update BuildAndTest.yml
    
    * tmp
    
    * Update BuildAndTest.yml
    
    * Update BuildAndTest.yml
    
    * Update hardhat.config.js
    
    * Update BuildAndTest.yml

---
+++ test/0testSuitCheck.js
@@ -0,0 +1,27 @@
+const { expect } = require("chai");
+var helper = require('./MainnetHelper');
+
+describe("Checking test suit state before running unit tests", function () {
+
+  let signer;
+    
+  beforeEach(async function () {
+      
+    // get signers
+    [signer, addr1, addr2, ...addrs] = await ethers.getSigners();
+    
+  });
+
+  it("Ensure mainnet fork is running", async function () {
+        
+    // loading USDT contract	
+    const usdtToken = new ethers.Contract(helper.USDT_ADDRESS, helper.USDT_ABI, signer)  
+    expect(usdtToken.address).to.equal(helper.USDT_ADDRESS);
+
+    // check decimals (USDT has 6 decimals)
+    let decimals = await usdtToken.decimals()
+    expect(decimals).to.equal(6);
+    })
+  });
+
+
