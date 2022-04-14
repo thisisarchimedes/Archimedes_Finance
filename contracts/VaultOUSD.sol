dc9df861b99895bfc6eb76767f6c249fa386a43f
yotamdaniel <yotam@archimedesfi.com>
2022-04-14 14:57:58 -0700

    Code clean up

--- contracts/VaultOUSD.sol
+++ contracts/VaultOUSD.sol
@@ -2,8 +2,6 @@
 
 pragma solidity 0.8.4;
 
-// import "../ERC20.sol";
-// import "../utils/SafeERC20.sol";
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import {IERC20} from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import {SafeERC20} from "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
