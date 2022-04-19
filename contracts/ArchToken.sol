cc44f5373f1b0f310582459e6c69699df7e56f01
Jason Stockman <jason@archimedesfi.com>
2022-04-19 11:41:31 -0700

    added a Treasury address to mint to

--- contracts/ArchToken.sol
+++ contracts/ArchToken.sol
@@ -6,8 +6,11 @@ import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 /// @title Archimedes Token
 /// @dev This is the contract for the Archimedes rewards token
 contract ArchToken is ERC20 {
+    // TODO set treasury address
+    address addressTreasury = msg.sender;
+
     constructor() ERC20("Archimedes Token", "ARCH") {
-        // premint 1,000,000 tokens to owner
-        _mint(msg.sender, 1000000 * 10**decimals());
+        // premint 100,000,000 tokens to owner
+        _mint(addressTreasury, 100000000 ether);
     }
 }
