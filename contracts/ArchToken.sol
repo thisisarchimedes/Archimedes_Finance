aec805ec5102a8d74ee237d4470b57c512d2f93e
Jason <jason@archimedesfi.com>
2022-04-21 15:49:22 -0700

    Add burn arch token (#24)
    * added burn functionality from openzeppelin
    
    * inherit ERC20Burnable.sol

--- contracts/ArchToken.sol
+++ contracts/ArchToken.sol
@@ -6,7 +6,7 @@ import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Burnable.sol";
 
 /// @title Archimedes Token
 /// @dev This is the contract for the Archimedes rewards token
-contract ArchToken is ERC20 {
+contract ArchToken is ERC20, ERC20Burnable {
     /// @notice We premint to the _addressTreasury in the constructor when deployed
     constructor(address _addressTreasury) ERC20("Archimedes Token", "ARCH") {
         _mint(_addressTreasury, 100000000 ether);
