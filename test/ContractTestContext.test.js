f09fb79877c01f8d2b9082179c419e335f3fd10a
yotamdaniel <102186705+yotamdaniel@users.noreply.github.com>
2022-04-26 13:49:26 -0700

    Contract test context
    * initial commit
    
    * working on test
    
    * DI attemp
    
    * Basic impl without DI
    
    * Fix out of gas error
    
    * Treasuty and origination fee implemented
    
    * Commit after live review
    
    * minor fixes, adding local sol compiler files to gitignore
    
    * Change solidity complier to 0.8.4
    
    * Initial basic context
    
    Co-authored-by: tomerXarchimedes <98904111+tomerXarchimedes@users.noreply.github.com>

---
+++ test/ContractTestContext.test.js
@@ -0,0 +1,35 @@
+const { expect } = require("chai");
+const mainnetHelper = require('./MainnetHelper');
+const { ContractTestContext } = require('./ContractTestContext');
+
+
+
+describe("ContractTestContext", function () {
+    let contractTestContext;
+
+    before(async function () {
+        mainnetHelper.helperResetNetwork(mainnetHelper.defaultBlockNumber)
+        contractTestContext = new ContractTestContext();
+        await contractTestContext.setup();
+    });
+
+    describe("When context is initialized", () => {
+        describe("Coordinator will be initialized", async function () {
+            let coordinator;
+
+            beforeEach(() => {
+                coordinator = contractTestContext.coordinator;
+            });
+    
+            it("coordinator has a LvUSDToken address", async function () {
+                expect(await coordinator.addressOfLvUSDToken()).to.equal(contractTestContext.lvUSD.address)
+            })
+    
+            it("coordinator has a OUSD contract address", async function () {
+                expect(await coordinator.addressOfVaultOUSDToken()).to.equal(contractTestContext.vault.address)
+            })
+        })
+    })
+
+
+})
