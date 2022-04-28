5f50d4b8f78423a7f6ce86e1f0eb70f9ea25d295
Corey Martin <coreyn.martin@gmail.com>
2022-04-28 12:23:19 -0700

    refactor(): enforce styleguide for lvUSD (#28)
    * refactor(): enforce styleguide for lvUSD
    
    * fix tests and reentrancy guard
    
    * fix spelling

--- contracts/LvUsdToken.sol
+++ contracts/LvUsdToken.sol
@@ -2,6 +2,7 @@
 pragma solidity 0.8.13;
 
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/security/ReentrancyGuard.sol";
 
 /// @title lvUSD token
 ///
@@ -9,22 +10,30 @@ import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 ///
 /// TODO: add access control and roles
 ///
-contract LvUSDToken is ERC20("Archimedes lvUSD", "lvUSD") {
+contract LvUSDToken is ERC20("Archimedes lvUSD", "lvUSD"), ReentrancyGuard {
+    /// @dev Sets the address of the current minter contract
+    /// Timelocked function (set candidate and change owner after 17,280 blocks ~3 days)
+    /// Emits MinterSet
+    ///
+    /// @param accounts the accounts to set.
+    function _setMinter(address[] calldata accounts) internal {}
+
     /// @dev Mints tokens to a recipient.
     ///
     /// This function reverts if the caller does not have the minter role.
     ///
     /// @param recipient the account to mint tokens to.
     /// @param amount    the amount of tokens to mint.
-    function mint(address recipient, uint256 amount) external {
+    function mint(address recipient, uint256 amount) nonReentrant external {
         _mint(recipient, amount);
     }
 
     /// @dev Sets the address of the current minter contract
-    /// Timelocked function (set cadidate and change owner after 17,280 blocks ~3 days)
+    /// Timelocked function (set candidate and change owner after 17,280 blocks ~3 days)
     /// Emits MinterSet
     ///
-    ///
     /// @param accounts the accounts to set.
-    function setMinter(address[] calldata accounts) external {}
+    function setMinter(address[] calldata accounts) nonReentrant external {
+        _setMinter(accounts);
+    }
 }
