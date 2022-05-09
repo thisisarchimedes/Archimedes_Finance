a484fd58175c2273895b6e6b17f59aab447ebe3f
Corey Martin <coreyn.martin@gmail.com>
2022-05-09 16:25:42 -0700

    Autogenerate contract types (#58)
    * autogenerate contract types
    
    * fix commands
    
    * ensure types generated before tests
    
    Co-authored-by: yotamdaniel <102186705+yotamdaniel@users.noreply.github.com>

--- test/ContractTestContext.ts
+++ test/ContractTestContext.ts
@@ -1,6 +1,18 @@
-import { ContractFactory } from "ethers";
+import { Contract, ContractFactory } from "ethers";
 import { ethers } from "hardhat";
 import { addressOUSD, abiOUSDToken } from "./MainnetHelper";
+import type {
+    Coordinator,
+    CDPosition,
+    VaultOUSD,
+    Exchanger,
+    LeverageEngine,
+    LeverageAllocator,
+    PositionToken,
+    ParameterStore,
+} from "../types/contracts";
+import type { LvUSDToken } from "../types/contracts/LvUsdToken.sol";
+import type { SignerWithAddress } from "@nomiclabs/hardhat-ethers/signers";
 
 async function getContractFactories (factoryNames: string[]): Promise<{ [K: string]: ContractFactory }> {
     const contracts = await Promise.all(
@@ -12,85 +24,96 @@ async function getContractFactories (factoryNames: string[]): Promise<{ [K: stri
     }), {});
 }
 
-export class ContractTestContext {
-    // addresses for different roles
-    owner;
-    addr1;
-    addr2;
-    addr3;
-    treasurySigner;
-
-    // Archimedes contracts
-    coordinator;
-    cdp;
-    vault;
-    lvUSD;
-    exchanger;
-    leverageEngine;
-    leverageAllocator;
-    positionToken;
-    parameterStore;
-
+export type ContractTestContext = {
+    coordinator: Coordinator;
+    owner: SignerWithAddress;
+    addr1: SignerWithAddress;
+    addr2: SignerWithAddress;
+    addr3: SignerWithAddress;
+    treasurySigner: SignerWithAddress;
+    // Archimedes deployed contract instances
+    cdp: CDPosition;
+    vault: VaultOUSD;
+    lvUSD: LvUSDToken;
+    exchanger: Exchanger;
+    leverageEngine: LeverageEngine;
+    leverageAllocator: LeverageAllocator;
+    positionToken: PositionToken;
+    parameterStore: ParameterStore;
     // External contracts
-    externalOUSD;
+    externalOUSD: Contract;
+}
 
-    async setup () {
-        [this.owner, this.addr1, this.addr2, this.treasurySigner, this.addr3] = await ethers.getSigners();
+export async function buildContractTestContext (): Promise<ContractTestContext> {
+    const context = {} as ContractTestContext;
 
-        const contracts = await getContractFactories([
-            "CDPosition",
-            "Exchanger",
-            "LeverageEngine",
-            "LeverageAllocator",
-            "PositionToken",
-            "ParameterStore",
-            "VaultOUSD",
-            "LvUSDToken",
-            "Coordinator",
-        ]);
+    [context.owner, context.addr1, context.addr2, context.treasurySigner, context.addr3] = await ethers.getSigners();
 
-        this.externalOUSD = new ethers.Contract(addressOUSD, abiOUSDToken, this.owner);
+    const contracts = await getContractFactories([
+        "CDPosition",
+        "Exchanger",
+        "LeverageEngine",
+        "LeverageAllocator",
+        "PositionToken",
+        "ParameterStore",
+        "VaultOUSD",
+        "LvUSDToken",
+        "Coordinator",
+    ]);
 
-        [
-            this.cdp,
-            this.exchanger,
-            this.leverageEngine,
-            this.leverageAllocator,
-            this.positionToken,
-            this.parameterStore,
-            this.vault,
-            this.lvUSD,
-        ] = await Promise.all([
-            contracts.CDPosition.deploy(),
-            contracts.Exchanger.deploy(),
-            contracts.LeverageEngine.deploy(this.owner.address),
-            contracts.LeverageAllocator.deploy(),
-            contracts.PositionToken.deploy(),
-            contracts.ParameterStore.deploy(),
-            contracts.VaultOUSD.deploy(this.externalOUSD.address, "VaultOUSD", "VOUSD"),
-            contracts.LvUSDToken.deploy(),
-        ]);
+    context.externalOUSD = new ethers.Contract(addressOUSD, abiOUSDToken, context.owner);
 
-        this.coordinator = await contracts.Coordinator.deploy();
+    [
+        context.cdp,
+        context.exchanger,
+        context.leverageEngine,
+        context.leverageAllocator,
+        context.positionToken,
+        context.parameterStore,
+        context.vault,
+        context.lvUSD,
+        context.coordinator,
+    ] = await Promise.all([
+        contracts.CDPosition.deploy(),
+        contracts.Exchanger.deploy(),
+        contracts.LeverageEngine.deploy(context.owner.address),
+        contracts.LeverageAllocator.deploy(),
+        contracts.PositionToken.deploy(),
+        contracts.ParameterStore.deploy(),
+        contracts.VaultOUSD.deploy(context.externalOUSD.address, "VaultOUSD", "VOUSD"),
+        contracts.LvUSDToken.deploy(),
+        contracts.Coordinator.deploy(),
+    ]) as [
+        CDPosition,
+        Exchanger,
+        LeverageEngine,
+        LeverageAllocator,
+        PositionToken,
+        ParameterStore,
+        VaultOUSD,
+        LvUSDToken,
+        Coordinator
+    ];
 
-        // Post init contracts
-        await Promise.all([
-            this.leverageEngine.init(
-                this.coordinator.address,
-                this.positionToken.address,
-                this.parameterStore.address,
-                this.leverageAllocator.address,
-            ),
-            this.exchanger.init(this.lvUSD.address, this.coordinator.address, this.externalOUSD.address),
-            this.parameterStore.init(this.treasurySigner.address),
-            this.coordinator.init(
-                this.lvUSD.address,
-                this.vault.address,
-                this.cdp.address,
-                this.externalOUSD.address,
-                this.exchanger.address,
-                this.parameterStore.address,
-            ),
-        ]);
-    }
+    // Post init contracts
+    await Promise.all([
+        context.leverageEngine.init(
+            context.coordinator.address,
+            context.positionToken.address,
+            context.parameterStore.address,
+            context.leverageAllocator.address,
+        ),
+        context.exchanger.init(context.lvUSD.address, context.coordinator.address, context.externalOUSD.address),
+        context.coordinator.init(
+            context.lvUSD.address,
+            context.vault.address,
+            context.cdp.address,
+            context.externalOUSD.address,
+            context.exchanger.address,
+            context.parameterStore.address,
+        ),
+        context.parameterStore.init(context.treasurySigner.address),
+    ]);
+
+    return context;
 }
