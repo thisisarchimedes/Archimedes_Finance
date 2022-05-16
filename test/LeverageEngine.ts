4d0f33ed1d2340d3f880c6b8024de0bbaaf8ff42
Corey Martin <coreyn.martin@gmail.com>
2022-05-16 13:45:51 -0700

    remove test - will be on AccessController test instead

--- test/LeverageEngine.ts
+++ test/LeverageEngine.ts
@@ -13,22 +13,6 @@ describe("LeverageEngine test suit", async function () {
         expect(r.leverageEngine).to.not.be.undefined;
     });
 
-    describe("admin role", async function () {
-        it("Should fail to init contract when called by different address from deployer", async function () {
-            const leContract = await ethers.getContractFactory("LeverageEngine");
-            const leverageEngine = await leContract.deploy(r.addr1.address);
-
-            await expect(
-                leverageEngine.init(
-                    r.coordinator.address,
-                    r.positionToken.address,
-                    r.parameterStore.address,
-                    r.leverageAllocator.address,
-                ),
-            ).to.be.revertedWith("onlyAdmin: Caller is not admin");
-        });
-    });
-
     describe("initialization", async function () {
         it("createLeveragedPosition should revert when not intiailized", async function () {
             const leContract = await ethers.getContractFactory("LeverageEngine");
