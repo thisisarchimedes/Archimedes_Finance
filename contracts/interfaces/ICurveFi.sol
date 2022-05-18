a9a127ec810ba681b345d5f370937eab41d3fbdd
Jason <jason@archimedesfi.com>
2022-05-17 19:12:31 -0700

    new interface from bia.is

--- contracts/interfaces/ICurveFi.sol
+++ contracts/interfaces/ICurveFi.sol
@@ -1,101 +1,191 @@
-pragma solidity ^0.5.0;
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
 
-contract ICurveFiCurve {
-    function get_virtual_price() external returns (uint256 out);
+interface ICurveFiCurve {
+    function initialize(
+        string calldata _name,
+        string calldata _symbol,
+        address _coin,
+        uint256 _decimals,
+        uint256 _A,
+        uint256 _fee,
+        address _admin
+    ) external;
 
-    function add_liquidity(uint256[2] calldata amounts, uint256 deadline) external;
+    function decimals() external view returns (uint256);
+
+    function transfer(address _to, uint256 _value) external returns (bool);
+
+    function transferFrom(
+        address _from,
+        address _to,
+        uint256 _value
+    ) external returns (bool);
+
+    function approve(address _spender, uint256 _value) external returns (bool);
+
+    function get_previous_balances() external view returns (uint256[2] calldata);
+
+    function get_balances() external view returns (uint256[2] calldata);
+
+    function get_twap_balances(
+        uint256[2] calldata _first_balances,
+        uint256[2] calldata _last_balances,
+        uint256 _time_elapsed
+    ) external view returns (uint256[2] calldata);
+
+    function get_price_cumulative_last() external view returns (uint256[2] calldata);
+
+    function admin_fee() external view returns (uint256);
+
+    function A() external view returns (uint256);
+
+    function A_precise() external view returns (uint256);
+
+    function get_virtual_price() external view returns (uint256);
+
+    function calc_token_amount(uint256[2] calldata _amounts, bool _is_deposit) external view returns (uint256);
+
+    function calc_token_amount(
+        uint256[2] calldata _amounts,
+        bool _is_deposit,
+        bool _previous
+    ) external view returns (uint256);
+
+    function add_liquidity(uint256[2] calldata _amounts, uint256 _min_mint_amount) external returns (uint256);
+
+    function add_liquidity(
+        uint256[2] calldata _amounts,
+        uint256 _min_mint_amount,
+        address _receiver
+    ) external returns (uint256);
 
     function get_dy(
         int128 i,
         int128 j,
         uint256 dx
-    ) external returns (uint256 out);
+    ) external view returns (uint256);
+
+    function get_dy(
+        int128 i,
+        int128 j,
+        uint256 dx,
+        uint256[2] calldata _balances
+    ) external view returns (uint256);
 
     function get_dy_underlying(
         int128 i,
         int128 j,
         uint256 dx
-    ) external returns (uint256 out);
+    ) external view returns (uint256);
 
-    function exchange(
+    function get_dy_underlying(
+        int128 i,
+        int128 j,
+        uint256 dx,
+        uint256[2] calldata _balances
+    ) external view returns (uint256);
+
+    function exchange1(
         int128 i,
         int128 j,
         uint256 dx,
         uint256 min_dy
-    ) external;
+    ) external returns (uint256);
 
     function exchange(
         int128 i,
         int128 j,
         uint256 dx,
         uint256 min_dy,
-        uint256 deadline
-    ) external;
+        address _receiver
+    ) external returns (uint256);
 
     function exchange_underlying(
         int128 i,
         int128 j,
         uint256 dx,
         uint256 min_dy
-    ) external;
+    ) external returns (uint256);
 
     function exchange_underlying(
         int128 i,
         int128 j,
         uint256 dx,
         uint256 min_dy,
-        uint256 deadline
-    ) external;
+        address _receiver
+    ) external returns (uint256);
+
+    function remove_liquidity(uint256 _burn_amount, uint256[2] calldata _min_amounts) external returns (uint256[2] calldata);
 
     function remove_liquidity(
-        uint256 _amount,
-        uint256 deadline,
-        uint256[2] calldata min_amounts
-    ) external;
+        uint256 _burn_amount,
+        uint256[2] calldata _min_amounts,
+        address _receiver
+    ) external returns (uint256[2] calldata);
 
-    function remove_liquidity_imbalance(uint256[2] calldata amounts, uint256 deadline) external;
+    function remove_liquidity_imbalance(uint256[2] calldata _amounts, uint256 _max_burn_amount) external returns (uint256);
 
-    function commit_new_parameters(
-        int128 amplification,
-        int128 new_fee,
-        int128 new_admin_fee
-    ) external;
+    function remove_liquidity_imbalance(
+        uint256[2] calldata _amounts,
+        uint256 _max_burn_amount,
+        address _receiver
+    ) external returns (uint256);
 
-    function apply_new_parameters() external;
+    function calc_withdraw_one_coin(uint256 _burn_amount, int128 i) external view returns (uint256);
 
-    function revert_new_parameters() external;
+    function calc_withdraw_one_coin(
+        uint256 _burn_amount,
+        int128 i,
+        bool _previous
+    ) external view returns (uint256);
 
-    function commit_transfer_ownership(address _owner) external;
+    function remove_liquidity_one_coin(
+        uint256 _burn_amount,
+        int128 i,
+        uint256 _min_received
+    ) external returns (uint256);
 
-    function apply_transfer_ownership() external;
+    function remove_liquidity_one_coin(
+        uint256 _burn_amount,
+        int128 i,
+        uint256 _min_received,
+        address _receiver
+    ) external returns (uint256);
 
-    function revert_transfer_ownership() external;
+    function ramp_A(uint256 _future_A, uint256 _future_time) external;
+
+    function stop_ramp_A() external;
+
+    function admin_balances(uint256 i) external view returns (uint256);
 
     function withdraw_admin_fees() external;
 
-    function coins(int128 arg0) external returns (address out);
+    function admin() external view returns (address);
 
-    function underlying_coins(int128 arg0) external returns (address out);
+    function coins(uint256 arg0) external view returns (address);
 
-    function balances(int128 arg0) external returns (uint256 out);
+    function balances(uint256 arg0) external view returns (uint256);
 
-    function A() external returns (int128 out);
+    function fee() external view returns (uint256);
 
-    function fee() external returns (int128 out);
+    function block_timestamp_last() external view returns (uint256);
 
-    function admin_fee() external returns (int128 out);
+    function initial_A() external view returns (uint256);
 
-    function owner() external returns (address out);
+    function future_A() external view returns (uint256);
 
-    function admin_actions_deadline() external returns (uint256 out);
+    function initial_A_time() external view returns (uint256);
 
-    function transfer_ownership_deadline() external returns (uint256 out);
+    function future_A_time() external view returns (uint256);
 
-    function future_A() external returns (int128 out);
+    function name() external view returns (string calldata);
 
-    function future_fee() external returns (int128 out);
+    function symbol() external view returns (string calldata);
 
-    function future_admin_fee() external returns (int128 out);
+    function balanceOf(address arg0) external view returns (uint256);
 
-    function future_owner() external returns (address out);
+    function allowance(address arg0, address arg1) external view returns (uint256);
+
+    function totalSupply() external view returns (uint256);
 }
