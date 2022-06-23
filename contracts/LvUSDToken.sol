f40eaa2ddfaaa2c0a12719ac8fbcdd753cd91c4c
yotamdaniel <yotam@archimedesfi.com>
2022-06-23 14:55:43 -0700

    Final changes to lvUSD

--- contracts/LvUSDToken.sol
+++ contracts/LvUSDToken.sol
@@ -8,11 +8,13 @@ import "@openzeppelin/contracts/access/AccessControl.sol";
 /// @dev This is the contract for the Archimedes lvUSD USD pegged stablecoin
 contract LvUSDToken is ERC20("Archimedes lvUSD", "lvUSD"), AccessControl {
     bytes32 public constant ADMIN_ROLE = keccak256("ADMIN_ROLE");
+    bytes32 public constant MINTER_ROLE = keccak256("MINTER_ROLE");
 
     address internal _mintingDestination = address(0);
 
     constructor(address admin) {
         _grantRole(ADMIN_ROLE, admin);
+        _grantRole(MINTER_ROLE, admin);
     }
 
     /// @dev Mints tokens to a recipient.
@@ -21,18 +23,29 @@ contract LvUSDToken is ERC20("Archimedes lvUSD", "lvUSD"), AccessControl {
     ///
     /// @param amount    the amount of tokens to mint.
     function mint(uint256 amount) external {
-        // Only admin (which is a timelock) can mint
-        require(hasRole(ADMIN_ROLE, msg.sender), "Caller is not an Admin");
+        // Only minter can mint
+        require(hasRole(MINTER_ROLE, msg.sender), "Caller is not Minter");
         _mint(_mintingDestination, amount);
     }
 
     /// @dev Sets a new admin
     ///
-    /// @param newAdmin the accounts to set.
+    /// @param newAdmin the account to set as admin.
     function setAdmin(address newAdmin) external {
+        require(msg.sender != newAdmin, "new Admin cant be same as old");
         require(hasRole(ADMIN_ROLE, msg.sender), "Caller is not an Admin");
         _grantRole(ADMIN_ROLE, newAdmin);
         _revokeRole(ADMIN_ROLE, msg.sender);
+    }
+
+    /// @dev Sets a new minter
+    ///
+    /// @param newMinter the account to set as minter.
+    function setMinter(address newMinter) external {
+        require(msg.sender != newMinter, "new Minter cant be same as old");
+        require(hasRole(ADMIN_ROLE, msg.sender), "Caller is not an Admin");
+        _grantRole(MINTER_ROLE, newMinter);
+        _revokeRole(MINTER_ROLE, msg.sender);
     }
 
     /// @dev change mint address
