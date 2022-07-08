b89d8404be3e9a734976d63017082c4a8ec1c4d3
yotamdaniel <yotam@archimedesfi.com>
2022-07-08 10:52:37 -0700

    Update ParameterStore.sol

--- contracts/ParameterStore.sol
+++ contracts/ParameterStore.sol
@@ -7,7 +7,6 @@ import {AccessController} from "./AccessController.sol";
 import "@openzeppelin/contracts-upgradeable/proxy/utils/UUPSUpgradeable.sol";
 import "@openzeppelin/contracts/utils/Strings.sol";
 
-
 /// @title ParameterStore is a contract for storing global parameters that can be modified by a privileged role
 /// @notice This contract (will be) proxy upgradable
 contract ParameterStore is AccessController, UUPSUpgradeable {
