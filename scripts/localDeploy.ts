843bb0951399dd45e43d26cab79a574a6f8a4201
yotamdaniel <yotam@archimedesfi.com>
2022-07-18 16:07:30 -0700

    Update localDeploy.ts

--- scripts/localDeploy.ts
+++ scripts/localDeploy.ts
@@ -162,102 +162,6 @@ export async function buildContractTestContext (): Promise<ContractTestContext> 
 
     return context;
 }
-// async function setupArchimedesContracts () {
-//     [context.owner, context.addr1, context.addr2, context.treasurySigner, context.addr3] = await signers;
-
-//     context.externalOUSD = new ethers.Contract(addressOUSD, abiOUSDToken, context.owner);
-//     context.externalUSDT = new ethers.Contract(addressUSDT, abiUSDTToken, context.owner);
-//     // eslint-disable-next-line @typescript-eslint/ban-ts-comment
-//     // @ts-ignore
-//     context.external3CRV = new ethers.Contract(address3CRV, abi3CRVToken, context.owner);
-
-//     const paramStoreFactory = await ethers.getContractFactory("ParameterStore");
-//     context.parameterStore = await hre.upgrades.deployProxy(paramStoreFactory, [], { kind: "uups" });
-
-//     const cdpFactory = await ethers.getContractFactory("CDPosition");
-//     context.cdp = await hre.upgrades.deployProxy(cdpFactory, [], { kind: "uups" });
-
-//     const coordinatorFactory = await ethers.getContractFactory("Coordinator");
-//     context.coordinator = await hre.upgrades.deployProxy(coordinatorFactory, [], { kind: "uups" });
-
-//     const exchangerFactory = await ethers.getContractFactory("Exchanger");
-//     context.exchanger = await hre.upgrades.deployProxy(exchangerFactory, [], { kind: "uups" });
-
-//     const leverageEngineFactory = await ethers.getContractFactory("LeverageEngine");
-//     context.leverageEngine = await hre.upgrades.deployProxy(leverageEngineFactory, [], { kind: "uups" });
-
-//     const positionTokenFactory = await ethers.getContractFactory("PositionToken");
-//     context.positionToken = await hre.upgrades.deployProxy(positionTokenFactory, [], { kind: "uups" });
-
-//     const vaultFactory = await ethers.getContractFactory("VaultOUSD");
-//     context.vault = await hre.upgrades.deployProxy(vaultFactory, [context.externalOUSD.address, "VaultOUSD", "VOUSD"], { kind: "uups" });
-
-//     const poolManagerFactory = await ethers.getContractFactory("PoolManager");
-//     context.poolManager = await hre.upgrades.deployProxy(poolManagerFactory, [], { kind: "uups" });
-
-//     // Give context.owner some funds:
-//     // expecting minter to be owner
-//     const ownerStartingLvUSDAmount = ethers.utils.parseUnits("1000.0");
-//     await context.lvUSD.setMintDestination(context.owner.address);
-//     await context.lvUSD.mint(ownerStartingLvUSDAmount);
-//     await helperSwapETHWith3CRV(context.owner, ethers.utils.parseUnits("3.0"));
-
-//     // Create a LVUSD3CRV pool and fund with "fundedPoolAmount" of each token
-//     context.curveLvUSDPool = await createAndFundMetapool(context.owner, context);
-//     // Setup pool with approval
-//     await context.lvUSD.approve(context.curveLvUSDPool.address, ownerStartingLvUSDAmount);
-
-//     await context.lvUSD.approve(context.exchanger.address, ethers.constants.MaxUint256);
-//     await context.lvUSD.approve(context.coordinator.address, ethers.constants.MaxUint256);
-
-//     // setup Dependencies
-//     await Promise.all([
-//         context.leverageEngine.setDependencies(
-//             context.coordinator.address,
-//             context.positionToken.address,
-//             context.parameterStore.address,
-//             context.archToken.address,
-//             context.externalOUSD.address,
-//         ),
-
-//         context.coordinator.setDependencies(
-//             context.lvUSD.address,
-//             context.vault.address,
-//             context.cdp.address,
-//             context.externalOUSD.address,
-//             context.exchanger.address,
-//             context.parameterStore.address,
-//             context.poolManager.address,
-//         ),
-
-//         context.exchanger.setDependencies(
-//             context.parameterStore.address,
-//             context.coordinator.address,
-//             context.lvUSD.address,
-//             context.externalOUSD.address,
-//             context.external3CRV.address,
-//             context.curveLvUSDPool.address,
-//             addressCurveOUSDPool,
-//         ),
-//         context.vault.setDependencies(context.parameterStore.address, context.externalOUSD.address),
-
-//         context.parameterStore.changeTreasuryAddress(context.treasurySigner.address),
-
-//         context.poolManager.setDependencies(
-//             context.parameterStore.address,
-//             context.coordinator.address,
-//             context.lvUSD.address,
-//             context.external3CRV.address,
-//             context.curveLvUSDPool.address),
-//     ]);
-//     // setup roles correctly
-//     await context.coordinator.setExecutive(context.leverageEngine.address);
-//     await context.positionToken.setExecutive(context.leverageEngine.address);
-
-//     await context.exchanger.setExecutive(context.coordinator.address);
-//     await context.vault.setExecutive(context.coordinator.address);
-//     await context.cdp.setExecutive(context.coordinator.address);
-// }
 
 async function verifyDeployment () {
     console.log("ParamStore value rebaseFeeRate = %s", await context.parameterStore.getRebaseFeeRate());
@@ -266,7 +170,6 @@ const deployScript = async () => {
     context = await buildContractTestContext();
     await fundLVUSD();
     await fundARCH();
-    // await setupArchimedesContracts();
     await verifyDeployment();
     await cleanup();
 };
