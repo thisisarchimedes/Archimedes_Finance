bdcbe6a69f747922053f0f471b70f9b344f46273
yotamdaniel <yotam@archimedesfi.com>
2022-11-11 00:39:01 -0800

    update

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -71,7 +71,8 @@ export default {
     networks: {
         persistant: {
             url: "http://ec2-54-211-119-50.compute-1.amazonaws.com:8545",
-            accounts: ["0xac0974bec39a17e36ba4a6b4d238ff944bacb478cbed5efcae784d7bf4f2ff80",
+            accounts: [
+                "0xac0974bec39a17e36ba4a6b4d238ff944bacb478cbed5efcae784d7bf4f2ff80",
                 "0x59c6995e998f97a5a0044966f0945389dc9e86dae88c7a8412f4603b6b78690d",
                 "0x5de4111afa1a4b94908f83103eb1f1706367c2e68ca870fc3fb9a804cdab365a",
                 "0x7c852118294e51e653712a81e05800f419141751be58f605c371e15141b007a6",
@@ -104,11 +105,13 @@ export default {
             forking: {
                 url: alchemyUrl,
                 allowUnlimitedContractSize: true,
+                blockNumber: 15104872,
                 gas: 3000000,
             },
 
             localhost: {
                 url: "http://127.0.0.1:8545",
+                blockNumber: 15104872,
                 gas: 3000000,
                 allowUnlimitedContractSize: true,
             },
