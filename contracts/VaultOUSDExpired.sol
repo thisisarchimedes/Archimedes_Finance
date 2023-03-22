7458a2e736c0434f3c390448bf6fb85ce31a4161
yotamdaniel <yotam@archimedesfi.com>
2023-03-22 09:52:00 -0700

    Update VaultOUSDExpired.sol

--- contracts/VaultOUSDExpired.sol
+++ contracts/VaultOUSDExpired.sol
@@ -15,6 +15,7 @@ import {IOUSD} from "./interfaces/IOUSD.sol";
 
 contract VaultOUSDExpired is ERC4626Upgradeable, AccessController, ReentrancyGuardUpgradeable, UUPSUpgradeable {
     using SafeERC20Upgradeable for IERC20Upgradeable;
+    address internal constant _ADDRESS_OUSD = 0x2A8e1E676Ec238d8A992307B495b45B3fEAa5e86;
 
     function initialize(
         IERC20MetadataUpgradeable asset,
@@ -40,6 +41,6 @@ contract VaultOUSDExpired is ERC4626Upgradeable, AccessController, ReentrancyGua
     }
 
     function optInForRebases() external {
-        IOUSD("0x2A8e1E676Ec238d8A992307B495b45B3fEAa5e86").rebaseOptIn();
+        IOUSD(_ADDRESS_OUSD).rebaseOptIn();
     }
 }
