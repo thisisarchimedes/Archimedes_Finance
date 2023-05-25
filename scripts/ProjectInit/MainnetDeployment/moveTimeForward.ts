663a682dc7d5d3bff00b1dffcb42e5b53f672c95
Jason <jason@archimedesfi.com>
2023-05-25 14:38:30 -0700

    better console logs and changed to just 32 days.

--- scripts/ProjectInit/MainnetDeployment/moveTimeForward.ts
+++ scripts/ProjectInit/MainnetDeployment/moveTimeForward.ts
@@ -20,28 +20,31 @@ import { deployOrGetAllContracts, verifyArcimedesEngine } from "./Helpers";
 import { time } from "@nomicfoundation/hardhat-network-helpers";
 import { abiUSDC } from "../../../test/ABIs";
 import { feeDisABI } from "../feeDisABI";
+import moment from "moment";
 
 const archAddress = "0x73C69d24ad28e2d43D03CBf35F79fE26EBDE1011";
 const feeDisAddress = "0xB12a775ac2811b32c26Dfde3101Fd2018105De36";
 
 async function main() {
-    // const days45InSeconds = 60 * 60 * 24 * 45; // 45 days in seconds
-    const days15InSeconds = 60 * 60 * 24 * 15; // 15 days in seconds
+    const oneDayInSeconds = 60 * 60 * 24; // 1 day in seconds
 
-    time.increase(days15InSeconds)
+    // Move forward 32 days
+    const daysToMove = 32;
+    time.increase(oneDayInSeconds * daysToMove);
+    console.log("moved forward " + daysToMove + " days");
 
     const signersToFund: SignerWithAddress[] = await ethers.getSigners();
-    const userDistrbutorSigner = signersToFund[0]
+    const userDistrbutorSigner = signersToFund[0];
     const feeDis = await ethers.getContractAt(feeDisABI, feeDisAddress, userDistrbutorSigner);
     const archToken = await ethers.getContractAt(abiUSDC, archAddress, userDistrbutorSigner);
 
     await archToken.approve(feeDisAddress, ValueStore.ONE_ETH);
 
     await feeDis.depositToken(archAddress, ValueStore.ONE_ETH);
-    console.log("deposited arch")
-    time.increase(days15InSeconds * 2)
+    console.log("deposited arch rewards");
 
-
+    const todayTimestamp = (await time.latest()) * 1000;
+    console.log("today is now: ", moment(todayTimestamp).format("MMM DD, YYYY"));
 }
 main().catch((error) => {
     console.error(error);
